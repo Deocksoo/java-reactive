90d790b483b7e253259ddac26fba73a2726ec5c4
deocksoo <dusol92@snu.ac.kr>
2019-12-29 16:33:29 +0900

    Add TestObserver test

--- src/test/java/ch1/StartRxJavaTest.java
+++ src/test/java/ch1/StartRxJavaTest.java
@@ -1,7 +1,20 @@
 package ch1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.reactivex.Observable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class StartRxJavaTest {
 
+    @Test
+    @DisplayName("TestObserver를 활용한 테스트 코드")
+    void testByJunit() {
+        String[] data = {"1", "2", "3"};
+        Observable<Integer> source = Observable.fromArray(data)
+                .map(Integer::parseInt);
+
+        Integer[] expected = {1, 2, 3};
+
+        source.test().assertResult(expected);
+    }
 }
